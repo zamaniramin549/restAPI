60b907fb540f16d9041dd23d907c8ddba671355d
Ramin Zamani <ramin@lizard.global>
2024-08-08 03:36:43 -0400

    created custome user profile

--- restAPI/profiles_api/models.py
+++ restAPI/profiles_api/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, 
+    PermissionsMixin, 
+    BaseUserManager
+    )
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password= None):
+        if not email:
+            raise ValueError('User must have an email address.')
+        
+        email= self.normalize_email(email)
+        user= self.model(email= email, first_name= first_name, last_name= last_name)
+        user.set_password(password)
+        user.save(using= self._db)
+        return user
+    
+    def create_superuser(self, email, first_name, last_name, password):
+        user= self.create_user(email, first_name, last_name, password)
+        user.is_superuser= True
+        user.is_staff= True
+        user.save(using= self._db)
+        
+     
+class User(AbstractBaseUser, PermissionsMixin):
+    email= models.EmailField(max_length= 255, unique= True)
+    first_name= models.CharField(max_length= 255)
+    last_name= models.CharField(max_length= 255)
+    is_active= models.BooleanField(default= True)
+    is_staff= models.BooleanField(default= False)
+    
+    objects= UserProfileManager()
+    
+    USERNAME_FIELD= 'email'
+    REQUIRED_FIELDS= ['first_name', 'last_name']
+    
+    def get_full_name(self):
+        return self.first_name + ' ' + self.last_name
+    
+    def get_first_name(self):
+        return self.name
+    
+    def __str__(self):
+        return self.name
+    
+    
